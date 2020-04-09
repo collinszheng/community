c771dc1f10d6dc4bb3ba2a3ad419560a33b6f2d4
uuuei <eliseproject@outlook.com>
2020-04-09 12:33:56 +0800

    try to add github login support

---
+++ src/main/java/top/elise/bbs/controller/AuthorizeController.java
@@ -0,0 +1,48 @@
+package top.elise.bbs.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import top.elise.bbs.dto.AccessTokenDTO;
+import top.elise.bbs.dto.GithubUser;
+import top.elise.bbs.provider.GithubProvider;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        if(user != null){
+            //登陆成功
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        }else {
+            //登录失败
+            return "redirect:/";
+        }
+    }
+}
